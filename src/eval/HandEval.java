7850b6b5c08537c44c912e922e645d25ee80826c
Alex Smith <trapo6789@gmail.com>
2026-07-12 21:57:55 -0400

    Started HandEval

--- src/eval/HandEval.java
+++ src/eval/HandEval.java
@@ -1,7 +1,16 @@
 package eval;
 import model.*;
 
+import java.util.ArrayList;
+
 public class HandEval {
     // TODO: Create a large list containing all cards and evaluate all possible hands
+    private ArrayList<Card> fullHand = new ArrayList<>();
+
+    public HandEval(Board board, Player player){
+        fullHand.addAll(board.getCommunityCards());
+        fullHand.addAll(player.getPlayerCards());
+    }
     
+
 }
